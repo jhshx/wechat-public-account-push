8f17de89b04bc872b3596db4246d43dbf334bc0b
jhshx <94682968+jhshx@users.noreply.github.com>
2023-11-17 13:00:25 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -55,9 +55,9 @@ const USER_CONFIG = {
 
   CALLBACK_USERS: [
     {
-      name: 'o_yk56mLDbdgEERnP32xu5gf03QI',
+      name: 'TIAmo',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o_yk56mLDbdgEERnP32xu5gf03QI',
     }
   ],
 
